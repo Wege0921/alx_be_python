70945cf2ef718240fb3bfc9147ace576d88d3939
Wege0921 <wegemuluneh@gmail.com>
2024-06-17 17:30:23 -0400

    fix the code

--- python_introduction/basic_operations.py
+++ python_introduction/basic_operations.py
@@ -6,8 +6,9 @@ sum = number1 + number2 # calculates the sum.
 difference = number1 - number2 #This is the diffrence. 
 product = number1*number2
 
-print("Addition of " + number1 +" and " + number2 +" is " +sum)
-print("Subtraction of " + number1 +" and " + number2 +" is " + difference)
-print("Multiplication of " + number1 +" and " + number2 +" is " + product)
+print(f"Addition of {number1} and {number2} is {sum}")
+print(f"Subtraction of {number1} and  {number2}  is {difference}")
+print(f"Multiplication of {number1} and  {number2}  is {product}")
+
 
 
