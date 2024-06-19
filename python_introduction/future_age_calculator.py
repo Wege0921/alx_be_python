671b09423f1d1aaa3f036b9e0edb80030b1699ed
Wege0921 <wegemuluneh@gmail.com>
2024-06-19 11:41:01 -0700

    futre age fix

--- python_introduction/future_age_calculator.py
+++ python_introduction/future_age_calculator.py
@@ -1,9 +1,9 @@
 # Prompt the user to input their current age
-age = int(input("How old are you? "))
+current_age = int(input("How old are you? "))
 
 # Calculate the age in the year 2050
-age_in_2050 = current_age + 27
+age = current_age + 27
 
 # Print the result
-print(f"In 2050, you will be {age_in_2050} years old.")
+print(f"In 2050, you will be {age} years old.")
 
